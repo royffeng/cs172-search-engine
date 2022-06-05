fad8246b7a99a98747fda352943dd5ba8ba67196
Jeana-T <jeana2019@gmail.com>
2022-06-05 02:56:45 -0700

    Initial button

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./App.css";
 import SearchBar from "./Components/SearchBar";
 import TweetDisplay from "./Components/TweetDisplay";
-import TweetData from "./sample.json";
 
 function App() {
   return (
     <div className="App">
       <SearchBar placeholder="Search..." />
-      <TweetDisplay data={TweetData} />
     </div>
   );
 }
--- src/frontend/src/Components/SearchBar.js
+++ src/frontend/src/Components/SearchBar.js
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
-import { TweetDisplay } from "./TweetDisplay";
+import TweetDisplay from "./TweetDisplay";
+import TweetData from "./sample.json";
+
 function SearchBar({ placeholder }) {
-  return (
-    <div className="search">
-        <div className="searchInputs">
-            <input type="text" placeholder={placeholder} />
-            <div className="searchIcon"> </div>
+    const [userInput, userInputData] = useState([]);
+    
+    const handleInput = (event) => {
+        const input  = event.target.value;
+        userInputData(input);
+        console.log(input)
+    };
+    
+    return (
+        <div className="search">
+            <div className="searchInputs">
+                <input type="text" placeholder={placeholder} onChange={handleInput} />
+                
+                <div className='btn'>
+                    <button class="button" onClick={handleInput}>Search!</button>
+                </div>
+            </div>
+
+        
+            {userInputData.length != 0 && (
+                <TweetDisplay data={TweetData} />
+            )}
         </div>
-    </div>
-  );
+    );
+
 }
 
 export default SearchBar;
