60f5f6caf233f19ac562a4393e831650339c7991
michael dong <65258149+toothnnail@users.noreply.github.com>
2022-06-05 22:40:53 -0700

    Update TweetDisplay.js
    added hyperlink

--- src/frontend/src/Components/TweetDisplay.js
+++ src/frontend/src/Components/TweetDisplay.js
@@ -8,7 +8,7 @@ function TweetDisplay( {data} ) {
                     return(
                         <div className='card'>
                             <h5 class="firstLine"> {value.name} </h5> 
-                            <div class="fadedText firstLine"> {value.screen_name} </div>
+                            <a class="linked firstLine" href={value.url} target="_blank"> {value.screen_name} </a>
                             <div class="fadedText firstLine"> {value.created_at} </div>
                             <p class="text"> {value.tweet_text} </p> 
                         </div>
